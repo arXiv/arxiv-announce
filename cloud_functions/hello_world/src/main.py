f04df0a42bdfbad1bd73d5e68e950fb1571b1cba
kyokukou <erin@arxiv.org>
2024-08-19 11:14:37 -0700

    switch from print statemetns to logging

--- cloud_functions/hello_world/src/main.py
+++ cloud_functions/hello_world/src/main.py
@@ -1,23 +1,23 @@
 import json
 import base64
-
+import logging
 import functions_framework
 from cloudevents.http import CloudEvent
 
 @functions_framework.http
 def hello_world_http(request):
-    print("a log")
+    logging.info("a log")
     return f"Hello world! data: {request}"
 
 @functions_framework.cloud_event
 def hello_world(cloud_event: CloudEvent):
     event_data=cloud_event.get_data()
-    print (f"Event Data: {event_data}")
-    print (f"Event Attributes: {cloud_event.get_attributes()}")
+    logging.info (f"Event Data: {event_data}")
+    logging.info (f"Event Attributes: {cloud_event.get_attributes()}")
 
-    print (f"msg: {event_data['message']}")
+    logging.info (f"msg: {event_data['message']}")
     data=base64.b64decode(event_data['message']['data']).decode()
-    print(f"Decoded: {data}")
+    logging.info(f"Decoded: {data}")
     info=json.loads(data)
-    print(f"JSON data: {info.get('foo')}")
+    logging.info(f"JSON data: {info.get('foo')}")
     return
