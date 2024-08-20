eda94756fa86d8f78cf5e755729c10a37e04991a
kyokukou <erin@arxiv.org>
2024-08-20 07:59:05 -0700

    change import order

--- cloud_functions/hello_world/src/main.py
+++ cloud_functions/hello_world/src/main.py
@@ -1,26 +1,10 @@
 import json
 import base64
-import logging
 from cloudevents.http import CloudEvent
-
-import traceback
-import types
-
-def addh_st(self, hdlr):
-     print(f"About to add {hdlr} at")
-     print("___________ stacktrace ____________")
-     traceback.print_stack()
-     print("__________ end stacktrace _________")
-     self.__class__.addHandler(self, hdlr)
-
-     
-root_h = logging.getLogger()
-root_h.addHandler = types.MethodType(addh_st, root_h)
-root_h.addHandler(logging.StreamHandler())
-
 
 import functions_framework
 functions_framework.setup_logging()
+import logging
 
 @functions_framework.http
 def hello_world_http(request):
